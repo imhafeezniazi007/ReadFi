6e9e66adf7793f250b6d8bb44c73114e65bbad8d
Hafeez Niazi <imhafeezkniazi@gmail.com>
2023-02-28 17:32:32 +0500

    Google authentication added

--- app/src/main/java/com/example/readfi/LoginActivity.java
+++ app/src/main/java/com/example/readfi/LoginActivity.java
@@ -4,21 +4,14 @@ import static android.content.ContentValues.TAG;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 import com.example.readfi.databinding.ActivityLoginBinding;
-import com.example.readfi.databinding.ActivitySignUpBinding;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -26,8 +19,6 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -40,7 +31,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
-
+        setContentView(binding.getRoot());
 
         auth = FirebaseAuth.getInstance();
         if (auth.getCurrentUser() != null) {
@@ -80,84 +71,54 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        binding.btnTemp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(LoginActivity.this, ProfileActivity.class);
-                startActivity(i);
-            }
-        });
-        binding.btnTemp2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(LoginActivity.this, SearchActivity.class);
-                startActivity(i);
-            }
-        });
-        binding.btnTemp3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(LoginActivity.this, NewsDetailsActivity.class);
-                startActivity(i);
-            }
-        });
-        binding.btnTemp4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //      Intent i = new Intent(LoginActivity.this, AdminPanelActivity.class);
-                //      startActivity(i);
-            }
-        });
     }
 
 
-        @Override
-        public void onActivityResult ( int requestCode, int resultCode, Intent data){
-            super.onActivityResult(requestCode, resultCode, data);
+    @Override
+    public void onActivityResult ( int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
 
-            // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
-            if (requestCode == RC_SIGN_IN) {
-                // The Task returned from this call is always completed, no need to attach
-                // a listener.
-                Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-                handleSignInResult(task);
-            }
-        }
-
-        private void handleSignInResult (Task < GoogleSignInAccount > completedTask) {
-            try {
-                GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-                GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
-                if (acct != null) {
-                    String personName = acct.getDisplayName();
-                    String personGivenName = acct.getGivenName();
-                    String personFamilyName = acct.getFamilyName();
-                    String personEmail = acct.getEmail();
-                    String personId = acct.getId();
-                    Uri personPhoto = acct.getPhotoUrl();
-                }
-
-                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
-            } catch (ApiException e) {
-                // The ApiException status code indicates the detailed failure reason.
-                // Please refer to the GoogleSignInStatusCodes class reference for more information.
-                Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
-            }
-        }
-
-
-        private boolean validateInput (String username, String password){
-            if (TextUtils.isEmpty(username)) {
-                binding.edittextUsername.setError("Username cannot be empty");
-                return false;
-            }
-
-            if (TextUtils.isEmpty(password)) {
-                binding.edittextPassword.setError("Password cannot be empty");
-                return false;
-            }
-
-            return true;
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
         }
     }
+
+    private void handleSignInResult (Task < GoogleSignInAccount > completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+            if (acct != null) {
+                String personName = acct.getDisplayName();
+                String personGivenName = acct.getGivenName();
+                String personFamilyName = acct.getFamilyName();
+                String personEmail = acct.getEmail();
+                String personId = acct.getId();
+                Uri personPhoto = acct.getPhotoUrl();
+            }
+
+            startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+        }
+    }
+
+
+    private boolean validateInput (String username, String password){
+        if (TextUtils.isEmpty(username)) {
+            binding.edittextUsername.setError("Username cannot be empty");
+            return false;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            binding.edittextPassword.setError("Password cannot be empty");
+            return false;
+        }
+
+        return true;
+    }
+}
 
